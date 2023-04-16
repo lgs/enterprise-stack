b6009fc8850e43ee1fe01dedaa5478d4bde62c1e
Luca G. Soave <luca.soave@gmail.com>
2023-04-16 17:38:42 +0200

    re-group src/routes, (payment) & (auth) layouts into (main-layout) add independent src/routes/(test) to group new 'Tailwind Elements' layouts & components experiment

---
+++ src/routes/(main-layout)/(payment)/pricing/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { enhance } from '$app/forms'
+	import { CheckCircle } from 'lucide-svelte'
+	import { 
+		PUBLIC_PRICE_LOOKUP_KEY_ONE_TIME,
+		PUBLIC_PRICE_LOOKUP_KEY_MONTHLY,  
+		PUBLIC_PRICE_LOOKUP_KEY_YEARLY
+		} from '$env/static/public'
+
+	export let prices:string [] = [
+		PUBLIC_PRICE_LOOKUP_KEY_ONE_TIME,
+		PUBLIC_PRICE_LOOKUP_KEY_MONTHLY,  
+		PUBLIC_PRICE_LOOKUP_KEY_YEARLY
+		]		
+</script>
+
+{#each prices as price}
+<section class="card m-auto mt-16 max-w-sm p-8">
+	<h2>Enterprise</h2>
+	<p class="description mt-8 text-gray-400">A plan that scales with you.</p>
+
+	<div class="mt-8">
+		<span class="mr-2 text-5xl font-extrabold">$10</span>
+		<span class="text-gray-400">/month</span>
+	</div>
+
+	<ol class="mt-8 space-y-4">
+		<li class="flex gap-4">
+			<CheckCircle />
+			<span>Individual configuration</span>
+		</li>
+		<li class="flex gap-4">
+			<CheckCircle />
+			<span>No hidden fees</span>
+		</li>
+		<li class="flex gap-4">
+			<CheckCircle />
+			<span>Free updates</span>
+		</li>
+		<li class="flex gap-4">
+			<CheckCircle />
+			<span>Email support</span>
+		</li>
+	</ol>
+
+	<form method="POST" action="?/checkout" use:enhance>
+		<input
+			type="hidden"
+			name="priceId"
+			value={PUBLIC_PRICE_LOOKUP_KEY_ONE_TIME}
+		/>
+		<button class="btn variant-filled-primary mt-16 w-full">Checkout</button>
+	</form>
+</section>
+{/each}
